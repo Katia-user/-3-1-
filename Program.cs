9cbe2e6a6ba5f022328cfd8bc07d28cfcd001a51
Катя <@DESKTOP-8CVJE59>
2024-10-17 08:37:52 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введiть, будь ласка, натуральне число n:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ви ввели число: " + n);
+
+        if (n > 0)
+        {
+            double sum = 0;
+
+            for (int i = 1; i <= n; i++) 
+            {
+                sum += 1.0 / i;
+            }
+            Console.WriteLine($"Сума s = {sum}");
+        }
+        else
+        {
+            Console.WriteLine("Введене число значення n не є допустимим натуральним числом");
+        }
+    }
+}
---
+++ ЛР3(2)/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        double x = 0.1;
+        double E = Math.Pow(10, -5);
+        double sum = 0;
+        int n = 1;
+        double item;
+        do
+        {
+            item = (Math.Pow(-1, n) * n * Math.Sqrt(n * x)) / Factorial(2 * n + 1);
+            sum += item;
+
+            Console.WriteLine($"Iтерацiя {n}:");
+            Console.WriteLine($"Член ряду: {item}");
+            Console.WriteLine($"Часткова сума: {sum}");
+            Console.WriteLine($"Абсолютна похибка: {Math.Abs(item / sum)}");
+
+            n++;
+        } while (Math.Abs(item / sum) >= E);
+
+        Console.WriteLine($"Величина накопиченої суми ряду S1 {sum}");
+        Console.WriteLine($"Номер iтерацiї n={n}");
+        Console.WriteLine($"Величина поточного ряду item={item}");
+        Console.WriteLine($"Досягнута на поточнiй iтерацiї похибка е={Math.Abs(item / sum)}");
+    }
+    static double Factorial(int n)
+    {
+        double result = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+}
+
